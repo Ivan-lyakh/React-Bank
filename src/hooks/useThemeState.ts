54fb8099420b0031b42e7b7b91de70690ba6eea9
Ivan <lyakhivan232@gmail.com>
2026-08-15 15:00:11 +0200

    fix theme token

--- src/hooks/useThemeState.ts
+++ src/hooks/useThemeState.ts
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 
 export const useThemeState = () => {
 
+
+
   const [theme, setTheme] = useState(() => {
-    return localStorage.getItem("theme") || "light";
+
+    return localStorage.getItem("themeReactBank") || "light";
   });
+
+  console.log(localStorage.getItem("themeReactBank"))
+
 
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
-    localStorage.setItem("theme", theme);
+    localStorage.setItem("themeReactBank", theme);
   }, [theme]);
 
   function toggleTheme() {
